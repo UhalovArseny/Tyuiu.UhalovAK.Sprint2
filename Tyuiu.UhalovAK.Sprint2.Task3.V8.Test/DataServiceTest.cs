aa85b95b1364d6b1931a9299812e216ed42eada0
Uhalov Arseny <akukhalov@gmail.com>
2024-10-18 04:27:21 +0500

    Task3

---
+++ Tyuiu.UhalovAK.Sprint2.Task3.V8.Test/DataServiceTest.cs
@@ -0,0 +1,47 @@
+using Tyuiu.UhalovAK.Sprint2.Task3.V8.Lib;
+namespace Tyuiu.UhalovAK.Sprint2.Task3.V8.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double res = ds.Calculate(x);
+            double wait = -4.588;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 0;
+            double res = ds.Calculate(x);
+            double wait = 0.75;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCondition3()
+        {
+            DataService ds = new DataService();
+            double x = -1;
+            double res = ds.Calculate(x);
+            double wait = 0.25;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCondition4()
+        {
+            DataService ds = new DataService();
+            double x = -15;
+            double res = ds.Calculate(x);
+            double wait = -164.667;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.UhalovAK.Sprint2.Task3.V8/Program.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.UhalovAK.Sprint2.Task3.V8.Lib;
+namespace Tyuiu.UhalovAK.Sprint2.Task3.V8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* Спринт #2                                                                *");
+            Console.WriteLine("* Тема: Вложенные операторы if - else                                      *");
+            Console.WriteLine("* Задание #3                                                               *");
+            Console.WriteLine("* Вариант #8                                                               *");
+            Console.WriteLine("* Выполнил: Ухалов Арсений Константинович | АСОиУБ-24-1                   *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                 *");
+            Console.WriteLine("* Написать программу, которая вычисляет требуемое значение функции Y с     *");
+            Console.WriteLine("* использованием вложенных оператор if-else, где пользователь вводит       *");
+            Console.WriteLine("* значение переменной X с клавиатуры.                                      *");
+            Console.WriteLine("*                                                                          *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+            Console.WriteLine("****************************************************************************");
+
+
+
+
+            Console.WriteLine("Введите значение переменной X: ");
+            double x = Convert.ToInt32(Console.ReadLine());
+
+            DataService ds = new DataService();
+            double res = ds.Calculate(x);
+
+
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("****************************************************************************");
+
+            Console.WriteLine("Значение функции = " + res);
+
+
+            Console.ReadKey();
+        }
+    }
+}
