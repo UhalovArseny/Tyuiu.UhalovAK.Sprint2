71644867e3af1b0052fef5d97e9d63a04f5d6514
Uhalov Arseny <akukhalov@gmail.com>
2024-10-18 01:43:19 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.UhalovAK.Sprint2.Task0.V7.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.UhalovAK.Sprint2.Task0.V7.Lib
+{
+    public class DataService : ISprint2Task0V7
+    {
+        public bool[] GetCompareOperations(int x, int y)
+        {
+            bool[] res = new bool[6];
+
+            res[0] = x + 372 == y;
+            res[1] = x + 372 != y;
+            res[2] = x < y;
+            res[3] = y > x;
+            res[4] = x <= y;
+            res[5] = x >= y;
+
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.UhalovAK.Sprint2.Task0.V7/Program.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.UhalovAK.Sprint2.Task0.V7.Lib;
+namespace Tyuiu.UhalovAK.Sprint2.Task0.V7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            int x = 103;
+            int y = 475;
+            bool[] res = new bool[6];
+            res = ds.GetCompareOperations(x, y);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("X = " + x);
+            Console.WriteLine("Y = " + y);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("# РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+                Console.ReadKey();
+            
+        }
+    }
+}
