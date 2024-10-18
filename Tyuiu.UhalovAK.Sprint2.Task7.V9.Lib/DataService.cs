971ee7d899f983c05871a4da8525c033adcde7bf
Uhalov Arseny <akukhalov@gmail.com>
2024-10-18 07:08:37 +0500

    Task7

--- Tyuiu.UhalovAK.Sprint2.Task7.V9.Lib/DataService.cs
+++ Tyuiu.UhalovAK.Sprint2.Task7.V9.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.UhalovAK.Sprint2.Task7.V9.Lib
         public bool CheckDotInShadedArea(double x, double y)
         {
             bool res;
-            if ((Math.Sin(x) <= 0.5) && (x > 0) && (x <= 0.5) && (y > 0) && (y <= 5))
+            if ((Math.Sin(x) <= 0.5) && (x > 0) && (x <= 0.5) && (y > 0) && (y <= 0.5))
             {
                 res = true;
             }
