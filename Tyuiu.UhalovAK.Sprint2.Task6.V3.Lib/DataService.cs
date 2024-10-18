91540dcf7737d32d386bf14742a492d429e1d512
Uhalov Arseny <akukhalov@gmail.com>
2024-10-18 06:39:00 +0500

    Task6

--- Tyuiu.UhalovAK.Sprint2.Task6.V3.Lib/DataService.cs
+++ Tyuiu.UhalovAK.Sprint2.Task6.V3.Lib/DataService.cs
@@ -8,13 +8,13 @@ namespace Tyuiu.UhalovAK.Sprint2.Task6.V3.Lib
         {
             switch (value)
             {
-                case 1: return "Понедельник";
-                case 2: return "Вторник";
-                case 3: return "Среда";
-                case 4: return "Четверг";
-                case 5: return "Пятница";
-                case 6: return "Суббота";
-                case 7: return "Воскресенье";
+                case 1: return "понедельник";
+                case 2: return "вторник";
+                case 3: return "среда";
+                case 4: return "четверг";
+                case 5: return "пятница";
+                case 6: return "суббота";
+                case 7: return "воскресенье";
                 default: throw new ArgumentException($"Дни недели дожны быть от1 до 7. Значение{value}");
 
             }
